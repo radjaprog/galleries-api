04ae47555a9a32418f21c087e20aff7ceee0b754
radjaprog <vlada.radjenovic@gmail.com>
2023-06-28 00:52:03 +0200

    laravel api part 3 models_jwt_requests_seededUsers

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 /**
@@ -18,10 +20,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('12345678'),
+            'accepted_terms' => fake()->boolean(true),
             'remember_token' => Str::random(10),
         ];
     }
