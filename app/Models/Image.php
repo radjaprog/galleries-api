db47114c28123eaf16729dc64e9d52519ce62d53
radjaprog <vlada.radjenovic@gmail.com>
2023-06-27 22:46:07 +0200

    laravel project api migrations + models

---
+++ app/Models/Image.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'imageUrl',
+        'gallery_id'
+    ];
+
+    public function gallery()
+    {
+        return $this->belongsTo(Gallery::class);
+    }
+}
