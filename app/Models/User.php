76d2247b1d68c75c95574dedca17d1a967ec96a0
radjaprog <vlada.radjenovic@gmail.com>
2023-06-29 19:32:01 +0200

    laravel api part 7 updated User and Comment model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,11 @@ class User extends Authenticatable implements JWTSubject
         'password' => 'hashed',
     ];
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
