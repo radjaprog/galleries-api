0822a5481ba9a33e74c1985430583b49b8191298
radjaprog <vlada.radjenovic@gmail.com>
2023-07-03 21:52:00 +0200

    routing adjustment

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('galleries', GalleriesController::class);
 Route::resource('images', ImageController::class);
 Route::resource('comments', CommentController::class);
 
+Route::controller(GalleriesController::class)->group(
+    function () {
+        Route::get('galleries', 'index');
+        Route::get('my-galleries', 'getMyGalleries');
+        Route::get('galleries/{Id}', 'show');
+        Route::put('galleries/{Id}', 'update');
+        Route::delete('galleries/{Id}', 'destroy');
+        Route::post('galleries', 'store');
+    }
+);
 
 Route::controller(AuthController::class)->group(
     function () {
         Route::post('login', 'login');
+        Route::post('refresh', 'refresh');
         Route::post('logout', 'logout');
         Route::post('register', 'register');
     }
