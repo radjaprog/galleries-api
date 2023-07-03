960ec95e0a850be85752170f8b33bc04cf569fa8
radjaprog <vlada.radjenovic@gmail.com>
2023-07-03 21:48:43 +0200

    galleries controller adjust GET, PUT, DESTROY

--- app/Http/Controllers/GalleriesController.php
+++ app/Http/Controllers/GalleriesController.php
@@ -2,66 +2,81 @@
 
 namespace App\Http\Controllers;
 
+// namespace Illuminate\Contracts\Auth;
+
 use App\Http\Requests\CreateGalleryRequest;
 use App\Http\Requests\UpdateGalleryRequest;
 use App\Models\Gallery;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Http\Request;
+use PHPOpenSourceSaver\JWTAuth\JWTAuth;
 
 class GalleriesController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $galleries = Gallery::all();
+        $query = Gallery::with('images');
+        $page = $request->query('page', 1);
 
-        return $galleries;
+        return Gallery::with('images')->paginate(Gallery::PER_PAGE, ['*'], 'page', $page);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function getMyGalleries(Request $request)
     {
-        //
+        if (!auth()->user()) {
+            return response()->json([
+                'message' => 'Must be logged in'
+            ], 401);
+        }
+        $page = $request->query('page', 1);
+
+        return Gallery::with(['comments'])->where('user_id', auth()->user()->id)->paginate(Gallery::PER_PAGE, ['*'], 'page', $page);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-
     public function store(CreateGalleryRequest $request)
     {
-
         $validatedData = $request->validated();
 
         $gallery = Gallery::create([
             'name' => $validatedData['name'],
             'content' => $validatedData['content'],
-            'user_id' => auth()->id()
+            'user_id' => Auth::id()
         ]);
+
+        if (isset($validatedData['images'])) {
+            $images = [];
+
+            foreach ($validatedData['images'] as $imageData) {
+                $image = Image::create([
+                    'image_url' => $imageData['image_url'],
+                    'gallery_id' => $gallery->id,
+                ]);
+
+                $images[] = $image;
+            }
+
+            $gallery->images()->saveMany($images);
+        }
 
         return $gallery;
     }
+
     /**
      * Display the specified resource.
      */
     public function show($id)
     {
-        $gallery = Gallery::findOrFail($id);
-
-        return $gallery;
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return Gallery::with(['images', 'comments', 'user'])->findOrFail($id);
     }
 
     /**
@@ -72,22 +87,38 @@ class GalleriesController extends Controller
         $validatedData = $request->validated();
 
         $gallery = Gallery::find($id);
-
         if ($gallery === null) {
-            return response("Please enter valid id, id{$id} does not exist, Response::HTTP_NOT_FOUND");
+            return response()->json([
+                'message' => 'Please enter valid ID, {$id} does not exist'
+            ], 404);
         } else {
             $gallery->update([
-                'name' => $validatedData['name'],
-                'content' => $validatedData['content'],
-                'user_id' => auth()->id()
+                'name' => request('name'),
+                'content' => request('content'),
+                'image_url' => request('image_url'),
+                'user_id' => Auth::id()
             ]);
-        };
-        $gallery->save();
 
-        return $gallery;
+            if (isset($validatedData['images'])) {
+                $images = [];
+
+                foreach ($validatedData['images'] as $imageData) {
+                    $image = Image::create([
+                        'image_url' => $imageData['image_url'],
+                        'gallery_id' => $gallery->id,
+                    ]);
+
+                    $images[] = $image;
+                }
+
+                $gallery->images()->saveMany($images);
+            }
+
+            $gallery->save();
+
+            return $gallery;
+        }
     }
-
-
 
 
     /**
@@ -95,14 +126,6 @@ class GalleriesController extends Controller
      */
     public function destroy($id)
     {
-        // $gallery = Gallery::findOrFail($id);
-        // $gallery->delete();
-
-        // return
-        //     response()->json([
-        //         "status" => "success",
-        //     ]);
-
         return response()->json(Gallery::findOrFail($id)->delete());
     }
 }
